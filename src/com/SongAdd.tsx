8d2696a6183f82545d6a71af075839ffe5606b26
Matt Currier <fingerskier@gmail.com>
2025-07-09 08:41:54 -0500

    Add basic Dexie integration (#26)

--- src/com/SongAdd.tsx
+++ src/com/SongAdd.tsx
@@ -1,3 +1,28 @@
+import { useState } from 'react'
+import { db, type Song } from '../lib/db'
+
 export default function SongAdd() {
-  return <p>Add Song</p>
+  const [name, setName] = useState('')
+  const [text, setText] = useState('')
+
+  async function handleSubmit(event: React.FormEvent<HTMLFormElement>) {
+    event.preventDefault()
+    await db.open()
+    const song: Song = {
+      id: crypto.randomUUID(),
+      name,
+      text,
+    }
+    await db.songs.add(song)
+    setName('')
+    setText('')
+  }
+
+  return <form onSubmit={handleSubmit}>
+    <label>Name <input value={name} onChange={e => setName(e.target.value)} /></label>
+    <br />
+    <textarea value={text} onChange={e => setText(e.target.value)} />
+    <br />
+    <button type='submit'>Save</button>
+  </form>
 }
--- src/com/Songs.tsx
+++ src/com/Songs.tsx
@@ -1,3 +1,25 @@
+import { useEffect, useState } from 'react'
+import { StateButton } from 'ygdrassil'
+import { db, type Song } from '../lib/db'
+
 export default function Songs() {
-  return <p>All Songs</p>
+  const [songs, setSongs] = useState<Song[]>([])
+
+  useEffect(() => {
+    async function load() {
+      await db.open()
+      const list = await db.songs.toArray()
+      setSongs(list)
+    }
+    load()
+  }, [])
+
+  return <div>
+    <h2>Songs</h2>
+    <ul>
+      {songs.map(song => <li key={song.id}>
+        <StateButton to='song-edit' id={song.id}>{song.name}</StateButton>
+      </li>)}
+    </ul>
+  </div>
 }
