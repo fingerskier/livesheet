6351cfb1b3375bba0a4a7dd19e6d83d1213f0a7b
Matt Currier <fingerskier@gmail.com>
2025-07-09 14:34:19 -0500

    feat: enable PWA support (#29)

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,44 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
+import { VitePWA } from 'vite-plugin-pwa'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    VitePWA({
+      registerType: 'autoUpdate',
+      manifestFilename: 'site.webmanifest',
+      includeAssets: [
+        'favicon.svg',
+        'favicon.ico',
+        'apple-touch-icon.png',
+        'favicon-96x96.png',
+        'robots.txt',
+      ],
+      manifest: {
+        name: 'Livesheet',
+        short_name: 'Livesheet',
+        theme_color: '#ecdec3',
+        background_color: '#ecdec3',
+        display: 'standalone',
+        icons: [
+          {
+            src: '/web-app-manifest-192x192.png',
+            sizes: '192x192',
+            type: 'image/png',
+            purpose: 'maskable',
+          },
+          {
+            src: '/web-app-manifest-512x512.png',
+            sizes: '512x512',
+            type: 'image/png',
+            purpose: 'maskable',
+          },
+        ],
+      },
+    }),
+  ],
   resolve: {
     alias: {
       '@': '/src',
